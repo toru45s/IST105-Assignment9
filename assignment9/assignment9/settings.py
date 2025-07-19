04830910158b2ce03d4d0aae6f619834a04aa129
Tohru Yaginuma <toru45s@gmail.com>
2025-07-18 17:19:31 -0700

    update hosts

--- assignment9/assignment9/settings.py
+++ assignment9/assignment9/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(n6n+72_wqjudme6b^fo@o6@7i8sx(z5hj_!2^2lxfsh0!(kwz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
